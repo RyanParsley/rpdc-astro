985f34d9a7027266d8050ab2856fb2419658e878
Ryan Parsley <RMParsley@gmail.com>
2023-04-09 14:58:29 -0400

    chore: add GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
+import partytown from "@astrojs/partytown";
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://ryanparsley.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		mdx(),
+		sitemap(),
+		partytown({
+		  // Adds dataLayer.push as a forwarding-event.
+		  config: {
+			forward: ["dataLayer.push"],
+		  },
+		}),
+	  ],
 });

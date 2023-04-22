19cf1d767ee771c9b331bb5213b8552bd1226792
Ryan Parsley <RMParsley@gmail.com>
2023-04-21 21:04:38 -0400

    chore: repair lint errors from generators

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,12 @@
 module.exports = {
-  extends: ["plugin:astro/recommended", "prettier"],
+  extends: [
+    "plugin:astro/recommended",
+    "prettier",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
   overrides: [
     {
       // Define the configuration for `.astro` file.
@@ -7,7 +14,6 @@ module.exports = {
       // Allows Astro components to be parsed.
       parser: "astro-eslint-parser",
       // Parse the script in `.astro` as TypeScript by adding the following configuration.
-      // It's the setting you need when using TypeScript.
       parserOptions: {
         parser: "@typescript-eslint/parser",
         extraFileExtensions: [".astro"],
